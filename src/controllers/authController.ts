fe5ca64d93e2005ff82e24c6c9404e01c05cb915
LukazDs <lukazds2016@gmail.com>
2023-01-06 19:43:19 -0300

    refactor: change devDependencies

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
+import { IUserSignUp } from '../utils/userUtils.js';
 
 export async function insertUser(req: Request, res: Response) {
-  const user = req.body;
+  const user: IUserSignUp = req.body;
 
   res.status(201).send(user);
 }
