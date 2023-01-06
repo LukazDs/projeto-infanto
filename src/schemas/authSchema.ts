339c2d1c4c40c6ce8574231e60e18f2cf7ed4c29
LukazDs <lukazds2016@gmail.com>
2023-01-06 20:17:54 -0300

    feat: creates schema that validates signup data

---
+++ src/schemas/authSchema.ts
@@ -0,0 +1,14 @@
+import joi from 'joi';
+import { IUserSignUp } from '../utils/userUtils';
+
+export const userSignupSchema = joi.object<IUserSignUp>({
+  name: joi.string().min(3).max(10).required().label('Provide a valid name!'),
+  imageUrl: joi.string().uri().required().label('Provide a valid url!'),
+  email: joi.string().email().required().label('Provide a valid email!'),
+  password: joi
+    .string()
+    .length(8)
+    .required()
+    .label('Provide a valid password!'),
+  confirmPassword: joi.ref('password'),
+});
