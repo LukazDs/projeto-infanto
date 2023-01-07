840ae1514edde197d5956c6b4cca2419640dec07
LukazDs <lukazds2016@gmail.com>
2023-01-07 12:03:47 -0300

    feat: insert user with encrypted password

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
 import { IUserSignUp } from '../utils/userUtils.js';
 import * as authService from '../services/authService.js';
-import { Users } from '@prisma/client';
 
 export async function insertUser(req: Request, res: Response) {
   const user: IUserSignUp = req.body;
+
   await authService.insertUser(user);
 
   res.sendStatus(201);
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import * as userRepository from '../repositories/userRepository.js';
 import { IUserSignUp } from '../utils/userUtils.js';
 
@@ -8,5 +9,13 @@ export async function insertUser(user: IUserSignUp) {
 }
 
 async function organizeUser(user: IUserSignUp) {
+  user.password = await encryptPassword(user.password);
   delete user.confirmPassword;
+}
+
+async function encryptPassword(password: string) {
+  const digits: number = Number(process.env.PASSWORD_DIGIT_BCRYPT);
+  const passwordHash = bcrypt.hashSync(password, digits);
+
+  return passwordHash;
 }
