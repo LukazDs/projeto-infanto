82891c1fb7f70305eee193a2ab2708a3c8aee2dc
LukazDs <lukazds2016@gmail.com>
2023-01-09 23:30:06 -0300

    feat: create token in jwt

---
+++ src/token/jwt.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+const SECRET: string = String(process.env.JWT_KEY);
+
+const verifyToken = (data: string) => {
+  try {
+    const dataToken = jwt.verify(data, SECRET);
+    return dataToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+export default { verifyToken };
