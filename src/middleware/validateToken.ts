68939e609c169eeca599d29989f5d6473647b89c
LukazDs <lukazds2016@gmail.com>
2023-01-12 23:59:36 -0300

    feat: validation token

---
+++ src/middleware/validateToken.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from '../token/jwt.js';
+
+async function checkToken(req: Request, res: Response, next: NextFunction) {
+  const { authorization } = req.headers;
+  const token: string = String(authorization?.replace('Bearer ', ''));
+
+  const verified = jwt.verifyToken(token);
+
+  if (!verified) {
+    return res.status(401).send('Token inválido!');
+  }
+
+  res.locals.verified = verified;
+
+  next();
+}
+
+export default checkToken;
--- src/routes/personageRouter.ts
+++ src/routes/personageRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import * as personageController from '../controllers/personageController.js';
 import * as validatePersonage from '../middleware/validatePersonage.js';
+import checkToken from '../middleware/validateToken.js';
 
 const personageRouter = Router();
 
 personageRouter.post(
   '/user/:userId/personage/insert',
+  checkToken,
   validatePersonage.validateRegister,
   personageController.insertPersonage,
 );
